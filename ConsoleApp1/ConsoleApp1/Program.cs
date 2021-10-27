1b8de1df43928f878d019ab8570ff90d6761c469
unknown <you@example.com>
2021-10-27 06:56:09 +0400

    methods

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+#region 1
+////int a, b;
+//a=Convert.ToInt32(Console.ReadLine());
+//b=Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine(sum(a,b));
+
+//static string sum  (int a,int b)
+//{
+//    string result = $"sum of two  number = {a + b}";
+//    return result;
+
+//}
+#endregion
+
+#region 2
+//int a, b;
+//a=Convert.ToInt32(Console.ReadLine());
+//b=Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine(cem(a,b));
+
+//static int cem (int a, int b)
+//{
+//    if (a<b)
+//    {
+//        int sum = 0;
+//        for (int i = a; i <= b; i++)
+//        {
+//            sum=sum+i;
+//        }
+//        return sum;
+//    }
+//    return 0;
+
+//}
+#endregion
